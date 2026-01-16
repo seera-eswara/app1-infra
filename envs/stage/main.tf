601037c6668f0b6574ae70b9fe7a7dd1f3612031
eswara <seeraeswar@gmail.com>
2026-01-15 20:05:17 -0500

    changes

--- envs/stage/main.tf
+++ envs/stage/main.tf
@@ -1,3 +1,10 @@
 module "aks" {
-  source  = "git::https://github.com/org/terraform-platform-modules.git//modules/aks?ref=v1.2.0"
+  source = "git::https://github.com/your-org/terraform-azure-modules.git//modules/aks?ref=v1.1.0"
+
+  name           = "app1-stg-aks"
+  location       = "eastus"
+  resource_group = "rg-app1-stg"
+
+  vm_size    = "Standard_D4s_v5"
+  node_count = 3
 }
