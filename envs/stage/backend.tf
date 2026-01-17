668dd2334d77869e0b33502f4b4bb91992c04dff
eswara <seeraeswar@gmail.com>
2026-01-17 13:12:43 -0500

    fix: correct backend resource group name to rg-tfstate

--- envs/stage/backend.tf
+++ envs/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.6.6"
   backend "azurerm" {
-    resource_group_name  = "rg-tf-state"
+    resource_group_name  = "rg-tfstate"
     storage_account_name = "tfstatelzqiaypb"
     container_name       = "tfstate"
     key                  = "app1/stage.tfstate"
