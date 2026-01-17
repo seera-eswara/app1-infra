4392794fe78700383189cfdce6ce50d4925f6573
eswara <seeraeswar@gmail.com>
2026-01-17 09:51:32 -0500

    fix: update module sources to use correct org (seera-eswara) and SSH URLs

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,12 +1,12 @@
 module "vnet" {
-  source = "git::https://github.com/org/terraform-azure-modules.git//modules/vnet?ref=1995a3f45ade7920e8756291a78fa32db057b4f5"
+  source = "git::ssh://git@github.com/seera-eswara/terraform-azure-modules.git//modules/vnet?ref=1995a3f45ade7920e8756291a78fa32db057b4f5"
 
   name     = "app1-vnet"
   location = "eastus"
 }
 
 module "aks" {
-  source = "git::https://github.com/org/terraform-azure-modules.git//modules/aks?ref=1995a3f45ade7920e8756291a78fa32db057b4f5"
+  source = "git::ssh://git@github.com/seera-eswara/terraform-azure-modules.git//modules/aks?ref=1995a3f45ade7920e8756291a78fa32db057b4f5"
 
   private_cluster = true
   sku_tier        = "Standard"
